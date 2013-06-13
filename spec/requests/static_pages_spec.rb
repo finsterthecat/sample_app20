d5668af2060bea02d8469704d53efad1daac5e47
Tony Brouwer <tonybrouwer@gmail.com>
2013-06-12 22:32:22 -0400

    chapter 11.2.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,6 +35,17 @@ describe "Static pages" do
       it "should display feed micropost count" do
         page.should have_selector("span", text: "2 microposts")
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
 
     describe "for micropost counts" do
